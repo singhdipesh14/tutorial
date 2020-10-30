b3825d96530d645622b9d55ecf468519ad308241
singhdipesh14 <58405253+singhdipesh14@users.noreply.github.com>
2020-10-30 14:23:15 +0530

    svg added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Jumbotron from "./Jumbotron";
 import Description from "./Description";
+import Card from "./Card";
 import FooterLink from "./FooterLink";
 export default class Home extends React.Component {
   render() {
@@ -9,6 +10,7 @@ export default class Home extends React.Component {
         <Jumbotron />
         <Description />
         <hr />
+        <Card />
         <FooterLink />
       </div>
     );
